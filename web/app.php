17197be4cb66eb8b80014631f98344ee7db21cee
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-08-05 22:37:41 +0200

    Created a Symfony2 Emptier application

---
+++ web/app.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../app/AppKernel.php';
+
+$kernel = new AppKernel('prod', false);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
