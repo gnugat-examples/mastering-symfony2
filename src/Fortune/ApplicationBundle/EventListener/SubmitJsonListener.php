b97b080c7ced19a48055fdbeed7412016cd5076f
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-09-01 22:58:56 +0200

    Handled errors

--- src/Fortune/ApplicationBundle/EventListener/SubmitJsonListener.php
+++ src/Fortune/ApplicationBundle/EventListener/SubmitJsonListener.php
@@ -1,8 +1,10 @@
 <?php
-// File: src\Fortune\ApplicationBundle\EventListener;
+// File: src/Fortune/ApplicationBundle/EventListener/SubmitJsonListener.php
 
 namespace Fortune\ApplicationBundle\EventListener;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class SubmitJsonListener
@@ -12,6 +14,12 @@ class SubmitJsonListener
         $request = $event->getRequest();
         $content = $request->getContent();
         $data = json_decode($content, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            $data = array('message' => 'Invalid or malformed JSON');
+            $response = new JsonResponse($data, Response::HTTP_BAD_REQUEST);
+            $event->setResponse($response);
+            $event->stopPropagation();
+        }
         $request->request->add($data ?: array());
     }
 }
